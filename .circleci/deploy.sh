01053a0002fe38a0e6ff00855080de52d323376e
Mohi <58784914+mrostamii@users.noreply.github.com>
2022-04-13 13:38:29 +0430

    add: cd - check

---
+++ .circleci/deploy.sh
@@ -0,0 +1,14 @@
+ssh -p $SSH_PORT -T $SSH_USER@$SSH_HOST <<EOA
+
+ssh -T $SSH_NESTED_HOST <<EOB
+
+git fetch
+git checkout $CIRCLE_BRANCH
+git pull
+
+echo "$WEB3_INFURA_PROJECT_ID" | base64 --decode > .env
+echo "$MAX_GAS_FEE" | base64 --decode >> .env
+
+EOB
+
+EOA
